12c9173d5e8465a82a48b450d4b7ab90f7576249
Matthew Holden <matthew.holden@mindbodyonline.com>
2013-06-14 13:34:51 -0700

    Ready 2 start

---
+++ Podfile
@@ -0,0 +1,3 @@
+xcodeproj 'Roomie.xcodeproj'
+pod 'MSCollectionViewCalendarLayout', '~> 0.0.2'
+pod 'AFNetworking', '~> 1.3.0'
---
+++ Pods/Pods-environment.h
@@ -0,0 +1,26 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// AFNetworking
+#define COCOAPODS_POD_AVAILABLE_AFNetworking
+#define COCOAPODS_VERSION_MAJOR_AFNetworking 1
+#define COCOAPODS_VERSION_MINOR_AFNetworking 3
+#define COCOAPODS_VERSION_PATCH_AFNetworking 0
+
+// CupertinoYankee
+#define COCOAPODS_POD_AVAILABLE_CupertinoYankee
+#define COCOAPODS_VERSION_MAJOR_CupertinoYankee 0
+#define COCOAPODS_VERSION_MINOR_CupertinoYankee 1
+#define COCOAPODS_VERSION_PATCH_CupertinoYankee 1
+
+// MSCollectionViewCalendarLayout
+#define COCOAPODS_POD_AVAILABLE_MSCollectionViewCalendarLayout
+#define COCOAPODS_VERSION_MAJOR_MSCollectionViewCalendarLayout 0
+#define COCOAPODS_VERSION_MINOR_MSCollectionViewCalendarLayout 0
+#define COCOAPODS_VERSION_PATCH_MSCollectionViewCalendarLayout 2
+
